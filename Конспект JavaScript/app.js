b6485dd989d251880982f2c98a066cac14b6648e
Alex-Torzuk <_alex__08@ukr.net>
2022-06-02 22:51:34 +0300

    +

--- Конспект JavaScript/app.js
+++ Конспект JavaScript/app.js
@@ -3200,35 +3200,35 @@ console.log(typeof arr[0])*/
 
 // console.log(result);
 
-function isFreindly(num1, num2) {
-	let sum1 = getSum(getOwnDivisors(num1));
-	let sum2 = getSum(getOwnDivisors(num2));
+// function isFreindly(num1, num2) {
+// 	let sum1 = getSum(getOwnDivisors(num1));
+// 	let sum2 = getSum(getOwnDivisors(num2));
 	
-	if (sum1 == num2 && sum2 == num1) {
-		return true;
-	} else {
-		return false;
-	}
-}
-console.log(isFreindly(220, 284))
+// 	if (sum1 == num2 && sum2 == num1) {
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// }
+// console.log(isFreindly(220, 284))
 
-function getOwnDivisors(num) {
-    let arr = []
-    for (let i = 1; i < num; i++) {
-        if (num % i == 0) {
-           arr.push(i)
-        }
-    }
-    return arr
-}
+// function getOwnDivisors(num) {
+//     let arr = []
+//     for (let i = 1; i < num; i++) {
+//         if (num % i == 0) {
+//            arr.push(i)
+//         }
+//     }
+//     return arr
+// }
 
-function getSum(arr) {
-    let sum = 0
-    for (let elem of arr) {
-        sum += elem
-    }
-    return sum
-}
+// function getSum(arr) {
+//     let sum = 0
+//     for (let elem of arr) {
+//         sum += elem
+//     }
+//     return sum
+// }
 
 
 
