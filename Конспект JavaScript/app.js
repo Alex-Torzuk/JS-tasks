364549043511ddc8bbc0250622133c75f4aad04b
Alex-Torzuk <_alex__08@ukr.net>
2022-06-11 01:12:58 +0300

    +

--- Конспект JavaScript/app.js
+++ Конспект JavaScript/app.js
@@ -3255,6 +3255,33 @@ console.log(typeof arr[0])*/
 
 // console.log( getInt(arr1, arr2) );
 
+//                                                    Разность массивов на JavaScript
+
+function getDiff(arr1, arr2){
+	let diff1 = getFirstDiff(arr1, arr2);
+	let diff2 = getFirstDiff(arr2, arr1);
+	
+	return [].concat( diff1, diff2 );
+}
+
+function getFirstDiff(arr1, arr2){
+	let result = [];
+	
+	for (let elem of arr1) {
+		if (!inArray(elem, arr2)) {
+			result.push(elem);
+		}
+	}
+	
+	return result;
+}
+
+function inArray(elem, arr){
+	return arr.indexOf(elem) !== -1;
+}
+
+console.log( getDiff([1, 2, 3], [2, 3, 4, 5]) ); // выведет [1, 4, 5]
+
 
 
 
